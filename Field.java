52c0027d7089d7a82db1a035a222dd8bddc393d9
vadym <fly82@ukr.net>
2017-07-18 15:17:59 +0200

    step2

--- Field.java
+++ Field.java
@@ -24,4 +24,20 @@ public class Field {
     public int[][] getMatrix() {
         return matrix;
     }
+
+    public void print() {
+
+    }
+
+    public void removeFullLines() {
+
+    }
+
+    public Integer getValue(int x, int y) {
+        return new Integer(0);
+    }
+
+    public void setValue(int x, int y, int value) {
+
+    }
 }
--- Figure.java
+++ Figure.java
@@ -2,4 +2,45 @@ package com.javarush.task.task22.task2213;
 
 
 public class Figure {
+    private int x;
+    private int y;
+    private int[][] matrix = new int[3][3];
+
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void left() {
+    }
+    public void right() {
+    }
+    public void down() {
+    }
+    public void up() {
+    }
+    public void rotate() {
+    }
+    public void downMaximum() {
+    }
+    public boolean isCurrentPositionAvailable() {
+        return true;
+    }
+    public void landed() {
+
+    }
+
 }
