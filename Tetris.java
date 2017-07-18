6c4725b4485b86c56f3f410775f877aeff574c11
vadym <fly82@ukr.net>
2017-07-18 14:52:31 +0200

    step1

---
+++ Tetris.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task22.task2213;
+
+
+public class Tetris {
+    private Field field;
+    private Figure figure;
+    static Tetris game;
+
+    public static void main(String[] args) {
+        game = new Tetris();
+        game.run();
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void run() {
+
+    }
+    public void step() {
+
+    }
+}
